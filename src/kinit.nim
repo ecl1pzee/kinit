4c3346a4b35ef402028c0ed6163e97655a3fac31
ecl1pzee <235312011+ecl1pzee@users.noreply.github.com>
2026-08-09 21:35:07 +0200

    base, cli args

--- src/kinit.nim
+++ src/kinit.nim
@@ -1,5 +1,24 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/os
 
-when isMainModule:
-  echo("Hello, World!")
+let args = commandLineParams()
+
+if args.len == 0:
+  echo "[!] No argument was given. Try -h."
+  quit(0)
+
+if args[0] == "-h":
+  echo "kinitctl v0.1"
+  echo "-------------"
+  echo "Usage: kinitctl [options] stop | start | restart | enable | disable [service]"
+  echo ""
+  echo "Options:"
+  echo "  -v --verbose   Be more verbose"
+  echo "  -q --quiet     Be quiet."
+  echo "  -v --version   Show version of kinitctl"
+  quit(0)
+
+if args.len < 2:
+  echo "[x] No service given or unknown argument. Quitting here."
+  quit(1)
+
+let service = args[1] # allow something like kinitctl enable foo
